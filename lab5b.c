be797709ff0024552336659dc08057c5ad297a12
notabelincoln <abejordandx@gmail.com>
2021-04-01 01:20:33 +0100

    Adjusted timing so that internal clock oscillates at about 100 Hz

--- lab5b.c
+++ lab5b.c
@@ -66,7 +66,7 @@ void init_pwm(void)
 
 	DDRB |= (1<<PB1);  //set OC1A as an output
 	OCR1A=19999;    //set initial compare at 50%
-	ICR1=39999U; // 8 MHz /40000/2 = PWM frequency = 100 Hz
+	ICR1=40755U; // 8 MHz /40000/2 = PWM frequency = 100 Hz
 	TCCR1A = (1<<COM1A1); //zeros in COM1B1,COM1B0,WGM11,WGM10  
 	//internal clock, no prescaler , PWM mode 8
 	TCCR1B = (1<<WGM13) | (1<<CS10);
